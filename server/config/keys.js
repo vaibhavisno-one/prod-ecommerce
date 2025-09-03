c1747966c81f2d286caa41ca3c83d0721761480c
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-09-03 23:21:40 +0530

    backend

---
+++ server/config/keys.js
@@ -0,0 +1,27 @@
+module.exports = {
+  app: {
+    name: 'Mern Ecommerce',
+    apiURL: `${process.env.BASE_API_URL}`,
+    clientURL: process.env.CLIENT_URL
+  },
+  port: process.env.PORT || 3000,
+  database: {
+    url: process.env.MONGO_URI
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    tokenLife: '7d'
+  },
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+    bucketName: process.env.AWS_BUCKET_NAME
+  }
+};
